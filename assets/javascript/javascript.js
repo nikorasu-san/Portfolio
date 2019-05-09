f049be0e6a2e4a52f44af816f88913ea47cf73ba
Liz Lemon <nikorasu.dx@gmail.com>
2019-05-09 08:06:53 -0500

    several changes to font and starting the resume section

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
 
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
 });
